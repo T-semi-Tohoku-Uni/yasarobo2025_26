157365d3a6ecc532257f47fed908e2f003a3620c
shibatakeigo <sagankeigo17@gmail.com>
2025-10-09 03:50:33 +0900

    Fixed

--- .github/inrof_ros_type.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd /ros_ws/src
-git clone https://github.com/keigo1216/inrof2025_ros_type.git
--- .github/ldlidar.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /ros_ws/src
-git clone https://github.com/keigo1216/ldrobot-lidar-ros2
-cd ldrobot-lidar-ros2
-git checkout humble
-cd script
-./create_udev_rules.sh
