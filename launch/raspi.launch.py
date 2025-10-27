4bdd77ec8b32b8fa9559dbb8161e58c00e27f1f2
shibatakeigo <sagankeigo17@gmail.com>
2025-10-27 18:21:06 +0900

    change mcl parameter

--- launch/raspi.launch.py
+++ launch/raspi.launch.py
@@ -123,6 +123,7 @@ def generate_launch_description():
         executable="mcl_node",
         output="screen",
         parameters=[{
+            "particleNum": 1000,
             "initial_x": x,
             "initial_y": y,
             "initial_theta": theata,
