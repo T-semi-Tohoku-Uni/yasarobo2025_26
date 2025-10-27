072011506ec61947f7e7408dd0cb5179173f10dc
shibatakeigo <sagankeigo17@gmail.com>
2025-10-28 05:07:01 +0900

    Added ball

--- launch/simulation.launch.py
+++ launch/simulation.launch.py
@@ -6,14 +6,16 @@ from launch.actions import ExecuteProcess, IncludeLaunchDescription, RegisterEve
 from launch.event_handlers import OnProcessExit, OnProcessStart
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
-
 from launch_ros.actions import Node
+import launch.logging
 
 import xacro
 import os
 import math
 
 def generate_launch_description():
+    logger = launch.logging.get_logger("simulation_launch")
+
     x = 0.25
     y = 0.25
     z = 0.30
@@ -44,6 +46,9 @@ def generate_launch_description():
     xacro.process_doc(doc)
     params = {'robot_description': doc.toxml()}
 
+    # load ball urdf file
+    ball_urdf_file = os.path.join(package_dir, "urdf", "ball.urdf")
+
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
@@ -53,8 +58,8 @@ def generate_launch_description():
             params,
             {"use_sim_time": use_sim_time}
         ]
-    ) 
-
+    )
+    
     # gazebo settings
     gzserver_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
@@ -79,6 +84,19 @@ def generate_launch_description():
                 ],
         output='screen',
         emulate_tty=True,
+    )
+
+    # TODO: create ball
+    ball_spawn_entity = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=[
+            '-file', str(ball_urdf_file),
+            '-entity', 'ball',
+            '-x', str(1.19),
+            '-y', str(1.6),
+            '-z', str(z)
+        ]
     )
 
     # rviz2 settings
@@ -198,7 +216,9 @@ def generate_launch_description():
     return LaunchDescription([
         SetEnvironmentVariable(name='RCUTILS_COLORIZED_OUTPUT', value='1'),
         node_robot_state_publisher,
+        # node_ball_state_publisher,
         spawn_entity,
+        ball_spawn_entity,
         rviz_node,
         map_server_cmd,
         start_lifecycle_manager_cmd,
