51412c2a779760d4f5ad9652d8e493c2d9d89288
shibatakeigo <sagankeigo17@gmail.com>
2025-10-29 21:42:33 +0900

    update parameter

--- launch/raspi.launch.py
+++ launch/raspi.launch.py
@@ -128,7 +128,7 @@ def generate_launch_description():
             "initial_y": y,
             "initial_theta": theata,
             "odomNoise1": 2.0,
-            "odomNoise2": 2.5,
+            "odomNoise2": 4.0,
             "odomNoise3": 2.0,
             "odomNoise4": 4.0,
             "resampleThreshold": 0.9,
