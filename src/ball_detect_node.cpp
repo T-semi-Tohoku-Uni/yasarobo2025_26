077f4a56fc4aab5319fc0b8a77a949e6e9d63dc4
shibatakeigo <sagankeigo17@gmail.com>
2025-11-02 18:03:36 +0900

    fixed lidar filter

--- src/ball_detect_node.cpp
+++ src/ball_detect_node.cpp
@@ -306,6 +306,14 @@ DBSCAN::PointCloud DBSCAN::BallDetect::scan2Point(const sensor_msgs::msg::LaserS
         if (is_sim_) theta = scan.angle_min + ((std::double_t)(i))*scan.angle_increment;
         else {
             theta = scan.angle_min + ((std::double_t)(i))*scan.angle_increment - 3.0*M_PI/2.0;
+            // normalize
+            if (theta > M_PI) theta -= M_2_PI;
+            if (theta < -M_PI) theta += M_2_PI;
+            
+            // clip
+            if (theta < -M_PI/2+1/10*M_PI) continue;
+            if (theta >  M_PI/2-1/10*M_PI) continue;
+
             // if (theta < (1/10)*M_PI || theta > (9/10)*M_PI) continue;
             RCLCPP_INFO(this->get_logger(), "%lf", theta);
         }
