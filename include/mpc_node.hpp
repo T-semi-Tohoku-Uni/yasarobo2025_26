677cc0e1274311a4b52bd1139448fa75acf88413
yamat <daikou5959@gmail.com>
2025-11-27 20:09:28 +0900

    add function

--- include/mpc_node.hpp
+++ include/mpc_node.hpp
@@ -14,12 +14,33 @@ namespace yasarobo2025_26{
 
         private:
             void poseCallback(const geometry_msgs::msg::Pose2D::SharedPtr msg);
-            void make_f();
+
+            casadi::SX inverseKinematics(casadi::SX v1, casadi::SX v2, casadi::SX v3);
+            casadi::SX compute_A(casadi::SX a, casadi::SX b, casadi::SX c);
+            casadi::SX compute_stage_cost(casadi::SX states, casadi::SX controls);
+            casadi::SX compute_terminal_cost(casadi::SX states); 
+            casadi::SX x_ref_ = casadi::SX::zeros(nx);
+            casadi::SX u_ref_ = casadi::SX::zeros(nu);
+            casadi::SX x_lb_;
+            casadi::SX u_lb_;
+            casadi::SX x_ub_;
+            casadi::SX u_ub_;
+            casadi::SX Q_;
+            casadi::SX R_;
+            casadi::SX Qf_;
+            casadi::Function make_f();
+            casadi::Function make_nlp();
+            
+            
 
             rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_pub_;
-            geometry_msgs::msg::Pose2D::SharedPtr pose_;
             rclcpp::Subscription<geometry_msgs::msg::Pose2D>::SharedPtr subpose_;
-            
+            geometry_msgs::msg::Pose2D::SharedPtr pose_;
+
+            int nx_ = 3;
+            int nu_ = 3;
+            int K_ = 10;
+            double r_ = 0.14;
             
     };
 }
